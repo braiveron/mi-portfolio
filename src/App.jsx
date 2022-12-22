da0007265f282c70368fc3681b8a3b7976f55faa
Braian Veron <braianveron88@gmail.com>
2022-12-22 01:44:12 -0300

    portfolio pronto para publicar, voy a agregar animaciones

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,12 @@ import Contacto from "./Components/Contacto/Contacto";
 import Info from "./Components/Info/Info";
 import ThemeProvider from "./Components/Theme/Themes";
 import Slider from "./Components/Slider/Slider";
-import NavPrueba from "./Components/NavPrueba";
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider>
         <Routes>
-          <Route exact path="/navprueba" element={<NavPrueba />} />
           <Route exact path="/" element={<Landing />} />
           <Route exact path="/mis-proyectos" element={<Proyectos />} />
           <Route exact path="/sobre-mi" element={<Info />} />
